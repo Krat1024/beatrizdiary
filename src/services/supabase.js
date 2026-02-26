569c26905be7c6f180925cdcf2b4c0d04650fa58
Krat1024 <j_ferburg@yahoo.com.br>
2026-02-25 23:17:34 -0300

    ajuste final das chaves

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // --- CONFIGURAÇÃO SUPABASE ---
 // Você pegará esses dados no painel do Supabase (Project Settings > API)
-const SUPABASE_URL = 'https://qdgwrpnitkbolnktspjy.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZ3dycG5pdGtib2xua3RzcGp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MzkwNTIsImV4cCI6MjA4NzMxNTA1Mn0.UxKZD4SyCRbM0zHAGNk8Zo9ckGLA4cN1tKhTa_x_Khw';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
